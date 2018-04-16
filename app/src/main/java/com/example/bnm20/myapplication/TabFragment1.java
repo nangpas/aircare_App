0b5049382349c6fe3084666afe91b8773a0b397c
nangpas <nangpas@naver.com>
2018-04-16 21:28:42 +0900

    0416

--- app/src/main/java/com/example/bnm20/myapplication/TabFragment1.java
+++ app/src/main/java/com/example/bnm20/myapplication/TabFragment1.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import static com.example.bnm20.myapplication.MainActivity.deviceHeight;
 import static com.example.bnm20.myapplication.MainActivity.deviceWidth;
@@ -30,8 +31,13 @@ public class TabFragment1 extends Fragment {
 
         View v = inflater.inflate(R.layout.tabfragment1, container, false);
 
-        //Drawable alpha = imageView.getDrawable();
-        //alpha.setAlpha(99);
+        imageView = (ImageView) v.findViewById(R.id.fragment1_weatherimage);
+        imageView.setImageResource(R.drawable.icon_weather_map09);
+        RelativeLayout.LayoutParams fragment1_weatherimage_params = (RelativeLayout.LayoutParams) imageView.getLayoutParams();
+        fragment1_weatherimage_params.height = deviceWidth/3;
+        fragment1_weatherimage_params.width = deviceWidth/3;
+        imageView.setLayoutParams(fragment1_weatherimage_params);
+
 
         return v;
     }
